9122eeb27b7cd9f3c36128aff47d941b706a16c9
issamkhalil <issam.khalil11@gmail.com>
2016-10-22 21:38:55 +0100

    title

--- assets/js/skils-modal.js
+++ assets/js/skils-modal.js
@@ -1,7 +1,19 @@
 $(".portfolioModal-img").click(function (e) {
-        console.log(e);
+    showContent($(this));
+    $("#pf-modal-media").html(
+        $("<img>").css("width", "100%").attr("src",
+            $(this).data("image-source")
+        )
+    )
 });
 
 $(".portfolioModal-vid").click(function (e) {
 
 });
+
+function showContent(elem) {
+    $("#pf-modal-txt").html(
+        elem.children(".pf-modal-content").html()
+    )
+    $("#portfolioModal").openModal();
+}
